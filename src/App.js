0b30ab40b23432ea582e439f3ebb11c49fc1c254
Zucker <muhammedkarakuza@gmail.com>
2023-12-11 17:19:07 +0300

    overflow and animation added

--- src/App.js
+++ src/App.js
@@ -13,8 +13,17 @@ const App = () => {
         {(data || []).map((item, i) => {
           const { photographer, src } = item;
           return (
-            <div className="galery" key={data.index}>
-              <img src={src.large} alt="" style={{ width: 300, height: 300 }} />
+            <div
+              className="galery col-xl-2 container col-md-3 col-xs-12"
+              key={data.index}
+            >
+              <div className="image">
+                <img
+                  src={src.large}
+                  alt=""
+                  style={{ width: 300, height: 300 }}
+                />
+              </div>
               <p>{photographer}</p>
             </div>
           );
@@ -25,4 +34,3 @@ const App = () => {
 };
 
 export default App;
-console.log("Data--> ", data);
